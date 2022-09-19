d622268792387641968a6980b904d7f00a79d278
Andrew Grenon <andrew.grenon@gmail.com>
2022-09-20 09:21:05 +1200

    hello-react-init-commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+
+function greet(personName) {
+  return { name: `Hello ${personName}` };
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{greet("John").name}</h1>
+      <h1>How are you </h1>
+      <h1>Today is a sunny rainy day</h1>
+    </>
   );
 }
 
